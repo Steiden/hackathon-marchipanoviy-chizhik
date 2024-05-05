3e8ac1fc69280c0504195d3e7f5d4ba870b0a218
Daniil <baldindaniilza@mail.ru>
2024-05-05 19:29:46 +0500

    Карточка

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,16 +1,33 @@
 import { TypeCard } from "@/types";
 import styles from "./Card.module.scss";
 
-export const Card = ({ name, phone_code, capital, region, subregion, nationality, timezones }: TypeCard) => {
-    return (
-        <div className={styles.card}>
-            <h1>{name}</h1>
-            <h2>{phone_code}</h2>
-            <h2>{capital}</h2>
-            <h2>{region}</h2>
-            <h2>{subregion}</h2>
-            <h2>{nationality}</h2>
-            <h2>{timezones}</h2>
-        </div>
-    )
-}
+export const Card = ({
+	name,
+	phone_code,
+	capital,
+	region,
+	subregion,
+	nationality,
+	timezones,
+}: TypeCard) => {
+	return (
+		<div className={styles.card}>
+			<h1>{name}</h1>
+			<h2>{phone_code}</h2>
+			<h2>{capital}</h2>
+			<h2>{region}</h2>
+			<h2>{subregion}</h2>
+			<h2>{nationality}</h2>
+			<div>
+				{timezones.map((timezone, index) => {
+					return (
+						<>
+							<h2 key={index}>{timezone.zoneName}</h2>
+							<h2 key={index}>{timezone.gmtOffsetName}</h2>
+						</>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
