910591e19bf6e52998ae8fa0340190153ea4f534
Daniil <baldindaniilza@mail.ru>
2024-05-05 20:25:20 +0500

    asdfa

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,48 +1,46 @@
 function generateResultCard(result) {
-  const card = document.createElement("div");
-  card.classList.add("result-card");
-  card.innerHTML = `
-        <h3>${result.title}</h3>
-        <p>${result.description}</p>
-        <a href="${result.link}" target="_blank">Подробнее</a>
+	const card = document.createElement("div");
+	card.classList.add("result-card");
+	card.innerHTML = `
+        <h3 class="result-title">${result.name}</h3>
+        <p class="result-label">State: ${result.state_name}</p>
+        <p class="result-label">Conutry: ${result.country_name}</p>
     `;
-  return card;
+	return card;
 }
 
-document
-  .getElementById("searchForm")
-  .addEventListener("submit", handleFormSubmit);
+document.getElementById("searchForm").addEventListener("submit", handleFormSubmit);
 
 async function handleFormSubmit(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  try {
-    const formData = new FormData(event.target);
-    let query = formData.get("query");
+	try {
+		const formData = new FormData(event.target);
+		let query = formData.get("query");
 
-    const response = await fetch("/search", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    });
+		const response = await fetch("/search", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				query: query,
+			}),
+		});
 
-    if (!response.ok) {
-      throw new Error("Error sending request: " + response.status);
-    }
+		if (!response.ok) {
+			throw new Error("Error sending request: " + response.status);
+		}
 
-    const data = await response.json();
+		const data = await response.json();
 
-    const resultsContainer = document.getElementById("results");
-    resultsContainer.innerHTML = "";
-    data.results.forEach((result) => {
-      const resultCard = generateResultCard(result);
-      resultsContainer.appendChild(resultCard);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+		const resultsContainer = document.getElementById("results");
+		resultsContainer.innerHTML = "";
+		data.results.forEach((result) => {
+			const resultCard = generateResultCard(result);
+			resultsContainer.appendChild(resultCard);
+		});
+	} catch (error) {
+		console.error(error);
+	}
 }
